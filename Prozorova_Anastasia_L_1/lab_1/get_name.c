e7f53a76d3703319020b87d32c8e4eb5cc269ee2
Inoretsu <inoretsu@yandex.ru>
2016-09-25 17:26:25 +0300

    #Prozorova_Anastasia_L_1 lab1 added

---
+++ Prozorova_Anastasia_L_1/lab_1/get_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+char* get_name(){
+    char* name = (char*)malloc(80*sizeof(char));
+    int i = 0;
+    char ch;
+    while ((ch = getchar()) != '\n')
+    {
+      name[i] = ch;
+      i++;
+    }
+    return name;
+}  
---
+++ Prozorova_Anastasia_L_1/lab_1/print_str.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void print_str(char* name) {
+printf ("%s", name); 
+}
+
