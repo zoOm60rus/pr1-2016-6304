da5446af27fc80236951fd097607c0213210f339
Vladimir <mrcitron@mail.ru>
2016-09-25 14:35:30 +0300

    L_1

---
+++ Ivanov_Vladimir/L_1/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char hello[90] = "Hello, ";
+    char* result;
+    result = get_name();
+    print_str(strncat(hello, result, 80));
+    free(result);
+    return 0;
+}
---
+++ Ivanov_Vladimir/L_1/print_str.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+void print_str(const char *name)
+{
+printf("%s\n", name);
+}
