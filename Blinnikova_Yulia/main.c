a386484a3b7d24d367fe6d84bfcf5413ebca564c
JoolsBlinnikova <ulyalichka83@yandex.ru>
2016-09-29 12:31:51 +0300

    Blinnikova_Yulia/

---
+++ Blinnikova_Yulia/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main(){
+    char hello[90] = "Hello, ";
+    char* result;
+    result = get_name();
+    print_str(strncat(hello, result, 80));
+    free(result);
+    return 0;
+}
---
+++ Blinnikova_Yulia_Lab_1/print_str.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+char* print_str(char*name){
+	printf("%s\n",name);
+}
+
+
