e06b08ad25c9ea5ad6b1337e0f0732ef3c87f268
PutkovDimi <putkov_99@mail.ru>
2016-09-29 12:08:46 +0300

    L_1

---
+++ lab_1/main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char hello[90] = "Hello, ";
+    char *result;
+    result = get_name();
+print_str(strncat(hello,result, 80));
+	free(result);
+   return 0;
+}
