e43f45309dd18471305052623a7e5528c85c588b
valeriaivankova98 <valeriaivankova94@gmail.com>
2016-10-06 00:35:03 +0300

    Update print_str.c

--- ivankova_valeria/L_1/print_str.c
+++ ivankova_valeria/L_1/print_str.c
@@ -1,9 +1,11 @@
-nclude<stdio.h>
+#include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
 
-char* print_str(char* name)
-{
-printf("%s\n",name);
-}
+  char*  print_str( char* name)
+                {
+                printf("%s\n", name);
+                return 0;
+                };
+
 
