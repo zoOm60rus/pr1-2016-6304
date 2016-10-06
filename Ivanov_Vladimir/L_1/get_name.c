3ef0d3ddb3fac019da64f4278a2562123133598e
Vladimir <mrcitron@mail.ru>
2016-10-06 10:16:09 +0300

    L_1(2)

--- Ivanov_Vladimir/L_1/get_name.c
+++ Ivanov_Vladimir/L_1/get_name.c
@@ -3,11 +3,11 @@
 #include <string.h>
 char* get_name(){
 char* name=(char*)malloc(80*sizeof(char));
-int i=0;
+int i = 0;
 char ch;
 while ((ch=getchar()) !='\n')
 {
-name[i]=ch;
+name[i] = ch;
 i++;
 }
 name[i]='\0';
