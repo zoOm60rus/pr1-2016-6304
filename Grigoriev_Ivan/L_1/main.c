f8c7300a944de8b20fdbeba3dd7b8e3d55794638
destrosw <grigorievivan.1@mail.ru>
2016-10-06 10:24:39 +0300

    sdad

--- Grigoriev_Ivan/L_1/main.c
+++ Grigoriev_Ivan/L_1/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <string.h>
-#include "get_name.h"
-#include "print_str.h"
 #include <stdlib.h>
-int main (){
-char hello[90] = "Hello, ";
+#include <string.h>
+#include "print_str.h"
+#include "get_name.h"
+int main(){
+char hello [90] = "Hello, ";
 char* result;
 result = get_name();
 print_str(strncat(hello, result, 80));
