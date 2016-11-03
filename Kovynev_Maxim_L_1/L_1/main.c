94300febd3c394a5707c6600977fd014ffaa70c5
kovinevmv <kovinevmv@gmail.com>
2016-11-04 00:25:46 +0300

    L_1

---
+++ Kovynev_Maxim_L_1/L_1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "print_str.h"
+#include "get_name.h"
+
+int main(){
+   
+ char hello[90] = "Hello, ";
+    char* result;
+    result = get_name();
+    print_str(strncat(hello, result, 80));
+    free(result);
+    return 0;
+}
+
---
+++ Kovynev_Maxim_L_1/L_1/print_str.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+  char*  print_str( char* name)
+                {
+                printf("%s\n", name);
+                return 0;
+                };
+
