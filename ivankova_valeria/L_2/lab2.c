2a82b206ceef4e6a44a304c6f20cb1434104ce53
valeriaivankova98 <valeriaivankova94@gmail.com>
2016-11-03 12:21:39 +0300

    Update lab2.c

--- ivankova_valeria/L_2/lab2.c
+++ ivankova_valeria/L_2/lab2.c
@@ -1,19 +1,19 @@
-void (int ch, int arr[], int n){ 
+void menu(int ch, int arr[], int n){ 
 if (n<=0) 
 { 
 printf("Данные некорректны"); 
 return; 
 } 
 else 
-switch (ch) 
+switch (ch) //Выполняет действия, основываясь на сравнении значения со списком констант
 { 
-case 0: printf("%d\n",get_max(arr,n)); 
+case 0: printf("%d\n",get_max(arr,n)); //Проверяет массив и выводит максимальное значение
 return; 
-case 1: printf("%d\n",get_min(arr,n)); 
+case 1: printf("%d\n",get_min(arr,n)); //Проверяет массив и выводит минималное значение
 return; 
-case 2: printf("%d\n",get_sum(arr,n)); 
+case 2: printf("%d\n",get_sum(arr,n)); //Проверяет массив и суммирует все четные числа
 return; 
-case 3: printf("%d\n",get_count_first_el(arr,n)); 
+case 3: printf("%d\n",get_count_first_el(arr,n)); //Проверяет массив и считает кол-во повторений
 return; 
 default: printf("Данные некорректны\n"); 
 } 
@@ -22,7 +22,7 @@ default: printf("Данные некорректны\n");
 
 int get_max(int arr[], int n){ 
 int max=arr[0]; 
-for(int i=1;i<n;i++) 
+for(int i=1;i<n;i++) //Задаем массив
 { 
 if(max<arr[i]) 
 max=arr[i]; 
