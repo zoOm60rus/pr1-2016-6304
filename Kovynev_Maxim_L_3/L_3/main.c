31a89fa6be6d6618d5796843a08ff0ea0ada5861
kovinevmv <kovinevmv@gmail.com>
2016-11-23 17:54:37 +0300

    Update L_3

--- Kovynev_Maxim_L_3/L_3/main.c
+++ Kovynev_Maxim_L_3/L_3/main.c
@@ -1,35 +1,27 @@
-#include "stdio.h"
-
+#include "stdio.h" //updated_version_2.0 (using_dinamic_arrays, minor_bug_fixes)
 
 void print(char* a, int len)
-{
-	int k, i, g = 0;
-	if (a[i] == ' ') { g = 1; }
-	else { g = 0; }
-	for (i = g; i < len;i++)
+{	for (int i = (a[0] == ' ') ? 1 : 0; i < len; i++)
 		printf("%c", a[i]);
 }
-
-int main()
-{
-	char c, buff[512];
-	int i = 0, k;
-	int m = 0, n = 0, len = 0;
-
+int main(){
+	char c; 
+	char* buf=NULL;
+	int m = 0, n = 0, len = 0, i = 0;
 	while ((c = getchar()) != '!')
 	{
 		switch (c)
 		{
-		case '.':  print(buff, len); printf("%c\n",c); i = 0; m++; n++; break;
-		case ';':  print(buff, len); printf("%c\n",c); i = 0; m++; n++; break;
-		case '?':  i = 0; m++; break;
-		case '\n': i = 0;	   break;
-		case '	': i = 0;	   break;
+		case '.':  print(buf, len); printf("%c\n", c); i = 0; m++; n++; break;
+		case ';':  print(buf, len); printf("%c\n", c); i = 0; m++; n++; break;
+		case '?':  i = 0; m++;  break;
+		case '\n': i = 0;	break;
+		case '\t': i = 0;	break;
 		default:
 		{
-			buff[i] = c;
-			i++;
-			len = i;
+			len = ++i;
+			buf = (char*)realloc(buf, i * sizeof(char));
+			buf[i - 1] = c; 
 		}
 		}
 	}
