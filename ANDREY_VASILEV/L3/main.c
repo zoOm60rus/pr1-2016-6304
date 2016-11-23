10cebffbdde8992abd93492e844cd711723db299
The-Drus <the-drus@yandex.ru>
2016-11-23 20:57:41 +0300

    Update main.c

--- ANDREY_VASILEV/L3/main.c
+++ ANDREY_VASILEV/L3/main.c
@@ -4,12 +4,13 @@
 int main()
 {
 	char* input=(char*)malloc(10000*sizeof(char));   				//Объявляем переменные и выделяем под них
-	int pered, posle, k, i, *index=(int*)malloc(500*sizeof(int));   //память
-	char output[500][1000];
-	vvod(input, index, &pered, &posle);		//Заполняем массив символов и получаем количество предл. до и после
+	int pered, posle, k, i, *index=(int*)malloc(500*sizeof(int));   	        //память
+	vvod(input, index, &pered, &posle);                 //Заполняем массив символов и получаем количество предл. до и после
+	char* output=(char*)malloc(strlen(input)*sizeof);	
 	redact(input, index, pered, output);	//Заполняем массив предложений и печатаем результат
 	free(input);							//
-	free(index);							//Освобождаем выделенную раннее память
+	free(index);
+	free(output);								//Освобождаем выделенную раннее память
 	printf("Количество предложений до %d и количество предложений после %d\n", pered, posle);
 }
 
