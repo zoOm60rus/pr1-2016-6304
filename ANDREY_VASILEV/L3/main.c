e2a3f11bde3da91c476efdeedb32bad54e233fdc
The-Drus <the-drus@yandex.ru>
2016-11-23 20:58:16 +0300

    Update main.c

--- ANDREY_VASILEV/L3/main.c
+++ ANDREY_VASILEV/L3/main.c
@@ -6,7 +6,7 @@ int main()
 	char* input=(char*)malloc(10000*sizeof(char));   				//Объявляем переменные и выделяем под них
 	int pered, posle, k, i, *index=(int*)malloc(500*sizeof(int));   	        //память
 	vvod(input, index, &pered, &posle);                 //Заполняем массив символов и получаем количество предл. до и после
-	char* output=(char*)malloc(strlen(input)*sizeof);	
+	char* output=(char*)malloc(strlen(input)*sizeof(char));	
 	redact(input, index, pered, output);	//Заполняем массив предложений и печатаем результат
 	free(input);							//
 	free(index);
