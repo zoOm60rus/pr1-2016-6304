113227ccb6a4ec49ca05c257356fcfe38a4106f2
kovinevmv <kovinevmv@gmail.com>
2016-12-10 17:42:18 +0300

    Add_comments

--- Kovynev_Maxim_Kursach/Kursach/print_names.c
+++ Kovynev_Maxim_Kursach/Kursach/print_names.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include "struct.h"
 
+/*Функция печати имен*/
 void print_names(MusicalComposition *head)
 {
+        /* Двигаем указатель до последнего элемента */
         while (head)
         {
+                /*Печать имени*/
                 printf("%s\n", head->name);
                 head = head->next;
         }
