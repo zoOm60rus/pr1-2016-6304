a7fe924939f5119396ca312a65cce8707a37df50
kovinevmv <kovinevmv@gmail.com>
2016-12-10 17:04:34 +0300

    Kursach

--- Kovynev_Maxim_Kursach/Kursach/struct.h
+++ Kovynev_Maxim_Kursach/Kursach/struct.h
@@ -1,10 +1,12 @@
+/* Структура "Музыкальная композиция */
 struct MusicalComposition{
 
-char name[80];
-char author[80];
-int year;
-struct MusicalComposition* next;
-struct MusicalComposition* prev;
+char name[80];    // Название композиции 
+char author[80];  // Автор композиции 
+int year;         // Год написания композиции 
+struct MusicalComposition* next; // Указатель на следующий элемент 
+struct MusicalComposition* prev; // Указатель на предыдущий элемент 
 };
-typedef struct MusicalComposition MusicalComposition;
+
+typedef struct MusicalComposition MusicalComposition; //Объявление нового типа данных
 
