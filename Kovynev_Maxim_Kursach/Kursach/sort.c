c8aa0f7660cd2f7850e36c2a6146cd1d4926f9fa
kovinevmv <kovinevmv@gmail.com>
2016-12-10 20:40:57 +0300

    Add_comments

--- Kovynev_Maxim_Kursach/Kursach/sort.c
+++ Kovynev_Maxim_Kursach/Kursach/sort.c
@@ -18,11 +18,11 @@ void sort(MusicalComposition *head)
 			{
 				/*Выделяем память под переменную temp, необходимую для обмена значений*/
 				MusicalComposition *temp = (MusicalComposition*)malloc(sizeof(MusicalComposition));
-				/*Обмениваемя данными с 2 элементами*/
+				/*Обмениваемся данными с 2 элементами*/
 				strcpy(temp->name, curr_j->next->name); strcpy(temp->author, curr_j->next->author); temp->year = curr_j->next->year;
 				strcpy(curr_j->next->name, curr_j->name); strcpy(curr_j->next->author, curr_j->author); curr_j->next->year = curr_j->year;
 				strcpy(curr_j->name, temp->name); strcpy(curr_j->author, temp->author); curr_j->year = temp->year;
-				/*Особождаем память*/
+				/*Освобождаем память*/
 				free(temp);
 			}
 			curr_j = curr_j->next;
