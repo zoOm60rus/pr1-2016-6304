d184d27d3061c25e3e29f0dc6621e686426b7d13
The-Drus <the-drus@yandex.ru>
2016-12-15 12:07:34 +0300

    Update removeEl.c

--- ANDREY_VASILEV/L4/removeEl.c
+++ ANDREY_VASILEV/L4/removeEl.c
@@ -1,26 +1,25 @@
-#include <stdlib.h>
-
-#include "struct.h"  //Подключаем файл с объявлением структуры
-
 void removeEl(MusicalComposition* head, char* name_for_remove)
 {
-    MusicalComposition *hold = NULL; //Создаем переменную и 
-    hold = head;			//сохраняем в ней "head".
-    while(head)										 //Цикл выполняется пока указатель не станет
-    {												 //станет равен "NULL".
-         if (strcmp(head->name, name_for_remove)==0) //Сравниваем строки и если они совпадают, то
-         {												//Присваиваем след. элементу. предыдущего 
-			 head->prev->next = head->next;				//указатель на следующий элемент текущего
-			 head->next->prev = head->prev;				//и предыдущему элементу следующего - 
-			 free(head);								//указатель на предыдущий эл. текущего.
-             break;										//Освобождаем память
-         }
-        head = head->next;				//Делаем текущим указатель на след. структуру
-    }
-    head = hold;  	//Возвращаем "head" исходное значение
+	MusicalComposition *hold = NULL; //Создаем переменную и 
+   	hold = head;			     //сохраняем в ней "head".
+    	while(head)	//Цикл выполняется пока указатель 
+    	{	       //станет равен "NULL".
+		if (strcmp(head->name, name_for_remove)==0) //Сравниваем строки 
+         	{	
+			if (head->next == NULL){   //Если элемент последний, то текущим
+				head = head->prev; //становится предыдущий элемент, а
+				head->next = NULL; //его указатель на след. становится NULL				break;
+			} else if (head->prev == NULL){	//Если удаляемый элемент первый, то
+				head->next->prev = NULL;	//"start" присваивается следующий
+				start = head->next;	//элемент списка, т.е.новое начало списка.
+			} else {
+//В случае, когда элемент является не крайним, перемещаем указатели данным образом
+				head->next->prev = head->prev;	
+				head->prev->next = head->next;
+			}
+								
+		}
+		head = head->next;	/*Если строки не совпали, делаем текущим указатель на след. структуру */
+	}
+    	head = hold;  	//Возвращаем "head" сохраненное значение
 }
-
-
-
-
-
