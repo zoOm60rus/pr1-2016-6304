a0f6af19b8df55e9b83315b6caecd384d17e94c2
kovinevmv <kovinevmv@gmail.com>
2016-12-21 20:01:36 +0300

    Delete createList.h

--- Kovynev_Maxim_Kursach/Kursach/createList.h
+++
@@ -1,4 +0,0 @@
-
-MusicalComposition* createMusicalComposition(char *name, char* author, int year);
-
-MusicalComposition* createMusicalCompositionList(char** array_names, char** array_authors, int* array_years, int n);
