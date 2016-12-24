c498d62b83d9dcf879db1cbfde5fb1ea21aad1e0
The-Drus <The-Drus@yandex.ru>
2016-12-24 22:08:53 +0300

    fix

--- ANDREY_VASILEV/L1/get_name.c
+++ ANDREY_VASILEV/L1/get_name.c
@@ -4,9 +4,10 @@ const char * get_name(char* name)
 	char ch;
 	int i = 0;
 	while (((ch = getchar()) != '\n'))
-		{
+	{
 		name[i] = ch;
 		i +=1;
-		}
+	}
+	name[i] = '\0';
 	return name;
 	}
--- ANDREY_VASILEV/L1/print_str.c
+++ ANDREY_VASILEV/L1/print_str.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-	void print_str(const char* name, const char* hello)
-		{
-		printf( "%s%s!\n", hello, name);
-		}
+void print_str(const char* name, const char* hello)
+{
+	printf( "%s%s!\n", hello, name);
+}
