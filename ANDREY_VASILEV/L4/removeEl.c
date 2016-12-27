380d65813618943f1ab3bfa9d4f4939c550d3a9c
The-Drus <the-drus@yandex.ru>
2016-12-27 22:46:21 +0300

    Update removeEl.c

--- ANDREY_VASILEV/L4/removeEl.c
+++ ANDREY_VASILEV/L4/removeEl.c
@@ -1,27 +1,30 @@
-void removeEl(MusicalComposition* head, char* name_for_remove)
+#include "struct.h"
+
+void removeEl(MusicalComposition** TrueHead, char* name_for_remove)
 {
-	MusicalComposition *hold = NULL; //Создаем переменную и 
-   	hold = head;			     //сохраняем в ней "head".
+	MusicalComposition *head = *TrueHead; //Создаем переменную и 
+   				         //сохраняем в ней "TrueHead".
     	while(head)	//Цикл выполняется пока указатель 
     	{	       //станет равен "NULL".
 		if (strcmp(head->name, name_for_remove)==0) //Сравниваем строки 
          	{	
 			if (head->next == NULL){   //Если элемент последний, то текущим
-				head = head->prev; //становится предыдущий элемент, а
-				head->next = NULL; //его указатель на след. становится NULL				break;
+				head->prev->next = NULL; //становится предыдущий элемент, а
+				free(head); //его указатель на след. становится NULL				break;
 				break;
 			} else if (head->prev == NULL){	//Если удаляемый элемент первый, то
 				head->next->prev = NULL;	//"hold" присваивается следующий
-				hold = head->next;	//элемент списка, т.е.новое начало списка.
+				*TrueHead = head->next;	//элемент списка, т.е.новое начало списка.
+				free(head);
 				break;
 			} else {
 //В случае, когда элемент является не крайним, перемещаем указатели данным образом
 				head->next->prev = head->prev;	
 				head->prev->next = head->next;
+				free(head);
 			}
 								
 		}
 		head = head->next;	/*Если строки не совпали, делаем текущим указатель на след. структуру */
 	}
-    	*head = *hold;  	//Возвращаем "head" сохраненное значение
 }
