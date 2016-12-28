d980e44fef3736ece9a1dee18e055521104dbd37
destrosw <grigorievivan.1@mail.ru>
2016-12-28 22:18:24 +0300

    Create api.h

---
+++ Grigoriev_kurswork/api.h
@@ -0,0 +1,7 @@
+typedef struct MusicalComposition MusicalComposition;
+MusicalComposition* createMusicalComposition(char* name, char* author, int year); 
+MusicalComposition* createMusicalCompositionList(char** array_names, char** array_authors, int* array_years, int n); 
+void push(MusicalComposition* head, MusicalComposition* element); 
+void removeEl(MusicalComposition* head, char* name_for_remove);
+int count(MusicalComposition* head); 
+void print_names(MusicalComposition* head); 
