be51292fcc62a018b74a0d4e0d5cb06566bed6fa
AleksandrRybin <Vfibyrf007@gmail.com>
2016-12-28 14:28:04 +0300

    Replace Lab2 correctly

---
+++ Rybin_Aleksandr/Lab2/Lab2.c
@@ -0,0 +1,66 @@
+void menu(int ch, int arr[], int n)
+{
+if (n<=0)
+    {  	
+   	 printf("Данные некорректны");
+   	 return;
+    }    
+    else 
+    	switch (ch)
+    {
+        case 0: printf("%d\n",get_max(arr,n));
+        return;
+        case 1: printf("%d\n",get_min(arr,n));
+        return;
+        case 2: printf("%d\n",get_sum(arr,n));
+        return;
+        case 3: printf("%d\n",get_count_first_el(arr,n));
+        return;
+        default: printf("Данные некорректны\n");
+    }
+}
+
+int get_max(int arr[], int n)
+{
+ int max=arr[0];
+    for(int i=1;i<n;i++)
+    {
+        if(max<arr[i])
+               max=arr[i];
+    }
+    return max;
+
+}
+
+int get_min(int arr[], int n)
+{
+ int min=arr[0];
+    for(int i=1;i<n;i++)
+    {
+        if(min>arr[i])
+               min=arr[i];
+    }
+    return min;
+}
+
+int get_sum(int arr[], int n)
+{  
+ int Sum=0;
+    for (int i=0;i<n;i++)
+    {
+        if (arr[i]%2==0)
+            Sum+=arr[i];
+     }
+return Sum;
+}
+
+int get_count_first_el(int arr[], int n)
+{
+int Sum=0;
+    for (int i=1;i<n;i++)
+    {
+      if (arr[i]==arr[0])
+            Sum++;
+    }
+    return Sum;
+}
