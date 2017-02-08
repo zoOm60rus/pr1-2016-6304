650f3ca16448ad68991bf327fbf34d684edd62e4
AleksandrRybin <Vfibyrf007@gmail.com>
2017-02-08 18:08:59 +0300

    Next task

---
+++ Задача 1.3.cpp
@@ -0,0 +1,32 @@
+// Задачи 08.02.2017.cpp : Defines the entry point for the console application.
+//
+
+// Задача 1.2
+#include "stdafx.h"
+#include "stdio.h"
+#include "windows.h"
+
+int main()
+{
+
+	char str[1000], c;
+
+	int count = 0;
+
+	printf(" Введите строку и искомый символ : \n");
+	scanf("%s %c", str, &c);
+
+	int len = strlen(str);
+
+	for (int i = 0; i < len; i++)
+	{
+		if (c == str[i])
+			count++;
+	}
+
+	printf("%d \n",count);
+	system("pause");
+
+    return 0;
+}
+
