e85ca9140d58c80695a6eec5e7595c62aea3154a
SergeyPopov123 <akerrus@gmail.com>
2017-02-12 00:38:21 +0300

    Update Task_4.c

--- POPOV_SERGEY_TEST/Task_4.c
+++ POPOV_SERGEY_TEST/Task_4.c
@@ -5,14 +5,16 @@
 int main()
 {
 	char c;
-	char* str = (char*)malloc(256*sizeof(char));
+	char str[256];
 	int i;
 	scanf("%c", &c);
 	scanf("%s", str);
 	for (i = 0; i<strlen(str); i++)
 	{
 		if (str[i]!=c)
-		printf("%c", str[i]);
+		{
+			printf("%c", str[i]);
+		}
 	}
 	printf("\n");
 	return 0;
