761d861dacf6d0974bcd5471ef7f59c91d06865b
kostya1703 <knock1703@gmail.com>
2017-02-14 21:53:31 +0300

    Create func.h

---
+++ kurs/func.h
@@ -0,0 +1,11 @@
+typedef struct MusicalComposition MusicalComposition;
+MusicalComposition* createMusicalComposition(char* name, char* author, int year);
+MusicalComposition* createMusicalCompositionList(char** array_names, char** array_authors, int* array_years, int n);
+void push(MusicalComposition* head, MusicalComposition* element);
+void removeEl(MusicalComposition* head, char* name_for_remove);
+int count(MusicalComposition* head);
+void print_names(MusicalComposition* head);
+void function(MusicalComposition* head);
+MusicalComposition* ChangeList2(MusicalComposition* head, int n, int m);
+void exchange(MusicalComposition* a, MusicalComposition* b);
+MusicalComposition* ChangeList1(MusicalComposition* head, int m);
