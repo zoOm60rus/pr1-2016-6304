8da1d2706a4b572ef8d86cb20359fe31efb63a33
chelentos <chelent0ss@gmail.com>
2017-03-14 19:37:16 +0300

    #Zalil lab1

---
+++ Timofeev_s2l1/lab.c
@@ -0,0 +1,38 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+
+int comp (const void * w1, const void * w2) //функция для сравнения элемента массива с искомым словом 
+{ 
+return strcmp((char*)w1, (char*)w2); //сравнение 2х указателей 
+} 
+
+int main() { 
+char txt[1000]; //массив на 1000 символов 
+char wtxt[1000][1000]; //массив(указателей) для записи отдельных слов 
+char sep[10]=" ."; //разделители 
+char str[30]; //искомое слово 
+char *ptxt,*bs; //указатель 
+int a=0; //счетчик 
+
+fgets(txt,1000,stdin); //считываем массив 
+fgets(str,30,stdin); //считываем слово 
+
+ptxt = strtok(txt,sep); //делим считанный массив на массив из строк, состоящих из отдельных слов 
+while (ptxt != NULL) { 
+strcpy(wtxt[a],ptxt); 
+ptxt = strtok (NULL,sep); 
+a++; 
+} 
+
+qsort(wtxt, a, 1000, comp); //сортируем слова 
+
+bs = (char*)bsearch(&str, wtxt, a, 1000, comp); // Если массив не содержит ключа, то возвращается нулевой указатель. 
+if (bs != NULL) 
+printf("exists\n"); 
+else 
+printf("doesn't exist\n"); 
+
+// put your code here 
+return 0; 
+}
