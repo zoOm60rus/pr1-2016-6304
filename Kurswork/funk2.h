c7f27c4c432defc5e95db87aee6778c5fe25d83a
SofiaVD <sofia.vasileva1995@gmail.com>
2017-05-07 13:41:42 +0300

    Deleted files

--- Kurswork/funk2.h
+++
@@ -1,4 +0,0 @@
-MusicalComposition* ChangeList(MusicalComposition* head);
-void exchange(MusicalComposition* a, MusicalComposition* b);
-MusicalComposition* ChangeList1(MusicalComposition* head, int m);
-MusicalComposition* ChangeList2(MusicalComposition* head, int n, int m);
