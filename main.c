2ef018d37c92cc7241f7fd3c15b4ae37cb887953
Tharitos <mischkane@mail.ru>
2017-05-17 18:11:19 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
+#define N 10
+int abs(int a){
+    if (a>0)return a;
+    else return -a;
+}
+int compare(const void* b, const void *a){
+    return abs(*(int*)a) - abs(*(int*)b);
+}
 int main()
 {
-    long int a, c=1, b, i;
-    scanf("%d %d",&a,&b);
-    for (i=0; i<b; i++)
-    {
-        c*=a;
+    int a[N]; int i; int time; int time1;
+    for (i=0; i<N; i++){
+        scanf("%d", &a[i]);
     }
-    printf("%d", c);
+    time = clock();
+    qsort(a, N, sizeof(int), compare);
+    time1 = clock() - time;
+    for (i=0; i<N; i++){
+        printf("%d ",a[i]);
+    }
+    printf("\n%d", time1);
     return 0;
 }
