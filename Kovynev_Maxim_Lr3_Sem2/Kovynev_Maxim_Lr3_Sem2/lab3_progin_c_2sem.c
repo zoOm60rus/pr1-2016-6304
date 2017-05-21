3f60956ecb32e2243bdede1dd79780aa66b61b89
kovinevmv <kovinevmv@gmail.com>
2017-05-21 14:32:46 +0300

    Add comments

--- Kovynev_Maxim_Lr3_Sem2/Kovynev_Maxim_Lr3_Sem2/lab3_progin_c_2sem.c
+++ Kovynev_Maxim_Lr3_Sem2/Kovynev_Maxim_Lr3_Sem2/lab3_progin_c_2sem.c
@@ -1,30 +1,78 @@
+/*
+ * Файл lab3_progin_c_2sem.c
+ * Обход дерева папок
+ * Автор: kovinevmv
+ * 21.05.2017
+*/
+
 #include <stdio.h>
-#include <sys/types.h>
 #include <dirent.h>
 #include <string.h>
 
+/*=============================================================
+  Функция readFile получает в качестве аргументов путь к файлу
+  current_path и имя папки folderName (add или mul), в которой 
+  находится файл. Функция выполняет определенные арифметические 
+  операции с числами из файла в зависимости от имени папки и 
+  возвращает результат.
+*/
+long long int readFile(char* current_path, char* folderName);
+//=============================================================
 
-long long int readFile(char* current_path, char* prev_path)
+
+/*=============================================================
+  Функция searchInDir получает в качестве аргументов текущий 
+  путь и папку, в которой находится текущий каталог (add или mul)
+  Если в директории есть другая директория, тогда рекурсивно 
+  вызывается функция searchInDir, которая добавляет или умножает
+  текущий результат на возвращаемое значение. Аналогичные действия 
+  выполняются если в директории есть файл TXT, только вызывается
+  функция readFile.
+*/
+long long int searchInDir(const char* path, char* prevFolderName);
+//=============================================================
+
+
+
+int main()
 {
-	int value = 0;
-	long long int result = 0;
-	int mathOperation;
-	FILE* fileTXT = fopen(current_path, "r");
+	printf("\nTotal result: %lld\n\n", searchInDir(".", ""));
+	return 0;
+}
 
-	if (!strcmp(prev_path, "mul"))
+
+
+long long int readFile(char* current_path, char* folderName)
+{
+
+	long long int result = 0;   // Результат выполнения операций
+
+	int mathOperation;     // Математическая операция, которая будет выполнена 
+						   // с числами из файла: 0 - сложение, 1 - умножение
+
+	// Распознавание математической операции
+	// mul - умножение, add - сложение
+	if (!strcmp(folderName, "mul"))
 	{
 		result = 1;
 		mathOperation = 1;
 	}
-	else if (!strcmp(prev_path, "add"))
+	else if (!strcmp(folderName, "add"))
 	{
 		result = 0;
 		mathOperation = 0;
 	}
 
+	// Открытие файла
+	FILE* fileTXT = fopen(current_path, "r");   
+
+	// Считываем каждое число пока файл не закончится 
 	while (!feof(fileTXT))
 	{
+		int value;
 		fscanf(fileTXT, "%d ", &value);
+
+		// Выполнение соответствующей операции
 		if (mathOperation)
 		{
 			result *= value;
@@ -36,23 +84,27 @@ long long int readFile(char* current_path, char* prev_path)
 
 	}
 
-	printf("Result: %lld\t File: %s\n",result, current_path);
+	// Вывод результата выполнения действий в файле, имеющего путь current_path
+	printf("Result: %lld\t File: %s\n", result, current_path);
+
 	fclose(fileTXT);
 	return result;
 
 }
 
-
-long long int searchDir(const char* path, char* prev_path)
+long long int searchInDir(const char* path, char* prevFolderName)
 {
 	long long int result = 0;
 	int mathOperation = 0;
-	if (!strcmp(prev_path, "mul"))
+
+	// Распознавание математической операции
+	// mul - умножение, add - сложение
+	if (!strcmp(prevFolderName, "mul"))
 	{
 		result = 1;
 		mathOperation = 1;
 	}
-	else if (!strcmp(prev_path, "add"))
+	else if (!strcmp(prevFolderName, "add"))
 	{
 		result = 0;
 		mathOperation = 0;
@@ -60,52 +112,61 @@ long long int searchDir(const char* path, char* prev_path)
 
 	char current_path[10000];
 	strcpy(current_path, path);
+
+	// Открываем текущую директорию
 	DIR *dir = opendir(current_path);
 	struct  dirent *de = readdir(dir);
+
+
 	if (dir)
 	{
 		while (de)
 		{
+
+			// current_path - полный путь к текущему файлу открытой директории
 			int path_len = strlen(current_path);
 			strcat(current_path, "/");
 			strcat(current_path, de->d_name);
+
+			// Если это не текущий и не родительский каталог, 
+			// то рекурсивный вызов функции обхода searchInDir
 			if (de->d_type == DT_DIR && strcmp(".", de->d_name) && strcmp("..", de->d_name))
 			{
 
 				if (mathOperation)
 				{
-					result *= searchDir(current_path, de->d_name);
+					result *= searchInDir(current_path, de->d_name);
 				}
 				else
 				{
-					result += searchDir(current_path, de->d_name);
+					result += searchInDir(current_path, de->d_name);
 				}
 
 			}
+
+			// Если это файл, оканчивающийся .txt, то
+			// вызов функции readFile 
 			else if (strstr(de->d_name, ".txt") && (de->d_type == 8))
 			{
 				if (mathOperation)
 				{
-					result *= readFile(current_path, prev_path);
+					result *= readFile(current_path, prevFolderName);
 				}
 				else
 				{
-					result += readFile(current_path, prev_path);
+					result += readFile(current_path, prevFolderName);
 				}
 
 			}
+
+			// Добавление символа конца строки
 			current_path[path_len] = '\0';
+
+			// Считывание нового элемента директории
 			de = readdir(dir);
 		}
 	}
 	closedir(dir);
 	return result;
 
-}
-
-int main()
-{
-
-	printf("\nTotal result: %lld\n\n", searchDir(".", ""));
-	return 0;
 }
